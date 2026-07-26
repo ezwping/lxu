0360f6e37a25b8e084a11b44e3ecb83722348e4e
ING <ezjie@outlook.com>
2026-07-26 15:23:18 +0800

    Add files via upload

---
+++ config/button.js
@@ -0,0 +1,15 @@
+window.BUTTON_CONFIG = [
+    { name: "现代极简案例", link: "https://链接1" },
+    { name: "现代简约案例", link: "https://链接2" },
+    { name: "意式轻奢案例", link: "https://链接3" },
+    { name: "宋式美学案例", link: "https://链接4" },
+    { name: "新 中 式 案 例", link: "https://链接5" },
+    { name: "中古神秘案例", link: "https://链接6" },
+
+    { name: "别墅大宅设计", link: "https://链接7" },
+    { name: "复式跃层设计", link: "https://链接8" },
+    { name: "平层整装案例", link: "https://链接9" },
+    { name: "办公空间方案", link: "https://链接10" },
+    { name: "餐饮门店设计", link: "https://链接11" },
+    { name: "娱乐休闲案例", link: "https://链接12" },
+];
